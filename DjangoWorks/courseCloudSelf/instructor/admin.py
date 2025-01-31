f462427f80e1d383c36ef238d28fbccefa166023
Razeem <rrazeem07@gmail.com>
2025-01-31 12:12:47 +0530

    LMS Project

---
+++ DjangoWorks/courseCloudSelf/instructor/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from instructor.models import User,Category,Course,Module,Lesson
+
+
+# Register your models here.
+admin.site.register(User)
+
+admin.site.register(Category)
+
+
+class LessonInline(admin.TabularInline):
+
+    exclude=("order",)
+
+    model=Lesson
+
+    extra=1
+
+class ModuleAdmin(admin.ModelAdmin):
+
+    exclude=("order",)
+
+    inlines=[LessonInline]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    exclude=("owner",)
+
+    def save_model(self,request,obj,form,change):
+
+        if not change:
+
+            obj.owner=request.user
+
+        return super().save_model(request,obj,form,change)
+
+
+admin.site.register(Course,CourseAdmin)
+
+
+# admin.site.register(Module)
+
+admin.site.register(Lesson)
+
+admin.site.register(Module,ModuleAdmin)
---
+++ DjangoWorks/courseCloudSelf/instructor/models.py
@@ -0,0 +1,129 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Max
+
+
+from embed_video.fields import EmbedVideoField
+
+
+# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_OPTIONS=(
+        ("instructor","instructor"),
+        ("student","student")
+    )
+
+    role=models.CharField(max_length=20,choices=ROLE_OPTIONS,default="student")
+
+
+
+
+class InstructorProfile(models.Model):
+
+    owner=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
+
+    expertise=models.CharField(max_length=200,null=True)
+
+    picture = models.ImageField(upload_to="profilepics",null=True,blank=True,default="profilepics/default.png")
+
+    description=models.CharField(max_length=200,null=True)
+
+
+from django.db.models.signals import post_save
+
+def create_instructor_profile(sender,instance,created,**kwargs):
+
+    if created and instance.role=="instructor":
+
+        InstructorProfile.objects.create(owner=instance)
+
+post_save.connect(create_instructor_profile,User)
+
+
+
+class Category(models.Model):
+
+    name=models.CharField(max_length=200,unique=True)
+
+    def __str__(self):
+
+        return self.name
+
+class Course(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    description=models.TextField()
+
+    price=models.DecimalField(decimal_places=2,max_digits=5)
+
+    owner=models.ForeignKey(User,on_delete=models.SET_NULL,related_name="courses",null=True)
+
+    is_free=models.BooleanField(default=False)
+
+    picture=models.ImageField(upload_to="courseimages",null=True,blank=True,default="courseimages/default.png")
+
+    thumbnail=EmbedVideoField()
+
+    category_objects=models.ManyToManyField(Category)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Module(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    course_object=models.ForeignKey(Course,on_delete=models.CASCADE,related_name="modules")
+
+    order=models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+    
+    def save(self,*args,**kwargs):
+
+        max_order=Module.objects.filter(course_object=self.module_object).aggregate(max=Max("order")).get("max") or 0
+
+        self.order=max_order+1
+
+        super().save(*args,**kwargs)
+
+    class Meta:
+
+        ordering=["order"]
+
+    
+
+class Lesson(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    module_object=models.ForeignKey(Module,on_delete=models.CASCADE,related_name="lessons")
+
+    video=EmbedVideoField(null=True)
+
+    order=models.PositiveIntegerField()
+
+    def __str__(self):
+        
+        return f"{self.module_object.title} + {self.title}"
+    
+    def save(self,*args,**kwargs):
+
+        max_order=Lesson.objects.filter(module_object=self.module_object).aggregate(max=Max("order")).get("max") or 0
+
+        self.order=max_order+1
+
+        super().save(*args,**kwargs)
+
+
+    
+     
